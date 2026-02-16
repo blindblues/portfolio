bdb75fec8b9ef3332997cb6d4959f7b47ec2fe48
blindblues <blindblues@users.noreply.github.com>
2026-02-16 17:46:31 +0100

    Fix CSP font paths and update site domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://blindblues.github.io',
+  site: 'https://emagre.blue',
   base: '/portfolio', // Assuming the repo name is 'portfolio'
   integrations: [react()]
 });
